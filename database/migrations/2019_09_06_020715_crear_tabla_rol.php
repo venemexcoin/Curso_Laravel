75bdcd8a4b0e0a08e12eb8e105bfef4dee211c06
venemexcoin <venemexcoin@gmail.com>
2019-09-06 00:33:54 -0500

    se crearon los archivos de Migracion del ER

--- database/migrations/2019_09_06_020715_crear_tabla_rol.php
+++ database/migrations/2019_09_06_020715_crear_tabla_rol.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFailedJobsTable extends Migration
+class CrearTablaRol extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('rol', function (Blueprint $table) {
             $table->Increments('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('nombre',50);
+            $table->timestamps();
         });
     }
 
@@ -30,6 +27,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('rol');
     }
 }
