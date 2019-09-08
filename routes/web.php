2139c206a0099138bc93c2fe7b010c6a9e71c996
venemexcoin <venemexcoin@gmail.com>
2019-09-08 01:46:48 -0500

    se exclulle los archivos de Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
-
-
-// Route::get('permiso/{nombre}', function($nombre){
-//     return $nombre;
-
-// })->where('nombre', '[A-Za-z]+')->name('permiso');
-
-
-Route::get('permiso/{nombre}', function($nombre){
-    return $nombre;
-
-})->where('nombre', '[0-9]+')->name('permiso');
